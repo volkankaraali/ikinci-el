b8706d3c0fa237dd263384cc72c5b6d75f9592f9
volkan karaali <volkankaraali1@gmail.com>
2022-05-05 23:38:29 +0300

    requireAuth- get token from cookie added

--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { useAuth } from '../context/AuthProviderContext';
+//import { useAuth } from '../context/AuthProviderContext';
+import useGetTokenFromCookie from '../hooks/useGetTokenFromCookie';
 
 
 function RequireAuth() {
-  const { auth } = useAuth();
+  //const { auth } = useAuth();
   const location = useLocation();
 
+  const token = useGetTokenFromCookie();
+
   return (
-    auth?.authToken ? <Outlet /> : <Navigate to='login' state={{ from: location }} replace  ></Navigate>
+    token ? <Outlet /> : <Navigate to='login' state={{ from: location }} replace></Navigate>
   );
 }
 
